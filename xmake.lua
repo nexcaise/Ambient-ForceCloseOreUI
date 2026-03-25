b9c2a33465c6a13987fad112bb0d5624d84fb729
AlvinQID <github@alvinq.my.id>
2026-03-25 07:04:27 +0700

    Add files via upload

--- xmake.lua
+++ xmake.lua
@@ -6,11 +6,15 @@ set_languages("cxx23")
 add_rules("mode.release")
 
 add_repositories(
-    "xmake-repo https://github.com/xmake-io/xmake-repo.git"
+    "xmake-repo https://github.com/xmake-io/xmake-repo.git",
+    "liteldev-repo https://github.com/LiteLDev/xmake-repo.git"
 )
+
+add_requires("nlohmann_json v3.11.3")
 
 target("ForceCloseOreUI")
     set_kind("shared")
+    add_packages("nlohmann_json")
     add_linkdirs("niseAPI/libs/arm64-v8a")
     add_linkdirs("miniAPI/libs/arm64-v8a")
     add_links("nise", "log", "miniAPI", "GlossHook")
