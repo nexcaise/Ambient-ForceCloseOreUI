a20060f2aac606d82e45779a0034683f08f05d84
alvinqid <github@alvinq.my.id>
2026-03-28 19:29:15 +0700

    fix or test!

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ void hooked_h2(
       saveJson(filePath, outputJson);
     }
 
-    original_h2(a1, a2, a3, a4, a5, a6, a7, a8, a9, ui, a11);
+    original_h2(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11);
 }
 
 hook_handle* g_hook = nullptr;
